efcf9f862e219617666d4ba00e7ad1b52dece887
aaronchun7 <aaronchun7@gmail.com>
2017-01-09 18:22:57 -0600

    updated the downcasting to bitmap

--- ImageComparer/ImageComparer.cs
+++ ImageComparer/ImageComparer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Drawing;
 using ColorMine.ColorSpaces;
@@ -11,20 +12,27 @@ namespace ImageComparer
         public double CompareImages(string voronoiPath, string originalImagePath)
         {
             var deltaEList = new List<double>();
-            var voronoiImage = (Bitmap)Image.FromFile(voronoiPath);
-            var originalImage = (Bitmap)Image.FromFile(originalImagePath);
-            for (var x = 0; x < voronoiImage.Width; x++)
+            var voronoiBitmap = Image.FromFile(voronoiPath) as Bitmap;
+            var originalBitmap = Image.FromFile(originalImagePath) as Bitmap;
+//            try
+//            {
+            for (var x = 0; x < voronoiBitmap.Width; x++)
             {
-                for (var y = 0; y < voronoiImage.Height; y++)
+                for (var y = 0; y < voronoiBitmap.Height; y++)
                 {
-                    var voronoiPixelColor = voronoiImage.GetPixel(x, y);
+                    var voronoiPixelColor = voronoiBitmap.GetPixel(x, y);
                     var voronoiRgb = new Rgb {R = voronoiPixelColor.R, B = voronoiPixelColor.B, G = voronoiPixelColor.G};
-                    var originalPixelColor = originalImage.GetPixel(x, y);
-                    var originalRgb = new Rgb { R = originalPixelColor.R, B = originalPixelColor.B, G = originalPixelColor.G };
+                    var originalPixelColor = originalBitmap.GetPixel(x, y);
+                    var originalRgb = new Rgb {R = originalPixelColor.R, B = originalPixelColor.B, G = originalPixelColor.G};
                     var deltaE = voronoiRgb.Compare(originalRgb, new CieDe2000Comparison());
                     deltaEList.Add(deltaE);
                 }
             }
+//            }
+//            catch (NullReferenceException nullRefExecption)
+//            {
+//                
+//            }
             // TODO remove this foreach loop later. use it to look at the values we get back
 //            foreach (var deltaE in deltaEList)
 //            {
