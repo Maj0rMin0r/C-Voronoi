1c64662229504ec471941103054952cdc4035141
aaronchun7 <aaronchun7@gmail.com>
2017-01-28 19:02:41 -0600

    fixing problems with files missing

---
+++ VoronoiTests/ImageComparerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VoronoiTests
+{
+    [TestClass]
+    public class ImageComparerTests
+    {
+        [TestMethod]
+        public void CompareImagesSameImageExpectZero()
+        {
+            var imageComparer = new ImageComparer.ImageComparer();
+            var averageDeltaE = imageComparer.CompareImages(@"C:\Users\aaron\Downloads\Turtle.png", @"C:\Users\aaron\Downloads\Turtle.png");
+            Assert.AreEqual(0.0, averageDeltaE);
+        }
+
+        [TestMethod]
+        public void CompareImagesDifferentImagesExpectNonZero()
+        {
+            var imageComparer = new ImageComparer.ImageComparer();
+            var averageDeltaE = imageComparer.CompareImages(@"C:\Users\aaron\Downloads\Turtle.png", @"C:\Users\aaron\Downloads\1401661448.png");
+            Assert.AreNotEqual(0.0, averageDeltaE);
+        }
+    }
+}
