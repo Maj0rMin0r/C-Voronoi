0dc48494abd213a1620a301dfc03812c2fef518f
Jim <kuczynskij@msoe.edu>
2017-02-03 16:22:03 -0600

    cleaned up graphedge

--- Voronoi/GraphEdge.cs
+++ Voronoi/GraphEdge.cs
@@ -1,4 +1,6 @@
-﻿namespace Voronoi
+﻿using System;
+
+namespace Voronoi
 {
     public class GraphEdge
     {
@@ -6,8 +8,10 @@
         public readonly Point2D P2;
         public readonly GraphEdge Next;
 
-        public GraphEdge(Point2D p1, Point2D p2, GraphEdge n)
+        internal GraphEdge(Point2D p1, Point2D p2, GraphEdge n)
         {
+            if (p1 == null) throw new ArgumentNullException(nameof(p1));
+            if (p2 == null) throw new ArgumentNullException(nameof(p2));
             P1 = p1;
             P2 = p2;
             Next = n;
