f8ee4d0e0197c588b07f3dfc347db7ffe5f176cd
aaronchun7 <aaronchun7@gmail.com>
2017-02-03 16:23:25 -0600

    updating naming convention variables

--- Voronoi/GraphEdge.cs
+++ Voronoi/GraphEdge.cs
@@ -6,7 +6,7 @@
         public readonly Point2D P2;
         public readonly GraphEdge Next;
 
-        public GraphEdge(Point2D p1, Point2D p2, GraphEdge n)
+        internal GraphEdge(Point2D p1, Point2D p2, GraphEdge n)
         {
             P1 = p1;
             P2 = p2;
